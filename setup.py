2e7e34dc65594539d7f691c7ad804e7b0d3f3753
paczyg <paczolay.gabor@gmail.com>
2025-07-09 15:42:41 +0000

    changed setup.py

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ setup(name='pantheonrl',
       include_package_data=True,
       install_requires=[
         'flask==2.2.2',
-        'tensorflow==2.11.0',
-        'torch==1.13.1',
-        'tensorboard==2.11.0',
-        'stable-baselines3==1.7.0',
-        'scipy==1.7.3',
-        'tqdm==4.64.1'
+        'tensorflow>=2.11.0',
+        'torch>=1.13.1',
+        'tensorboard>=2.11.0',
+        'stable-baselines3>=1.7.0',
+        'scipy>=1.7.3',
+        'tqdm>=4.64.1'
       ],
       )
